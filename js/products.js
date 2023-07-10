951686b4d48828ddf6de46926c4223553d5dbcdf
David Schöen <Davidschoennn6@gmail.com>
2023-07-10 10:44:35 -0300

    terminando detalles

---
+++ js/products.js
@@ -0,0 +1,15 @@
+const productos = [
+    {id:1 , nombre:"Plato Dragon", precio: 5500, cantidad:1, img:"img/Platos/drag.jpg"},
+    {id:2, nombre:"Plato Dragon X2", precio: 8000, cantidad:1, img:"img/Platos/dragone.jpg"},
+    {id:3, nombre:"Plato Guys", precio:5500, cantidad:1, img:"img/Platos/guys.jpg"},
+    {id:4, nombre:"Plato Onepi", precio:5500, cantidad:1, img:"img/Platos/onepi.jpg"},
+    {id:5, nombre:"Set Onepi", precio:10000, cantidad:1, img:"img/SetAnime/onepis.jpg"},
+    {id:6, nombre:"Set Osito", precio:12000, cantidad:1, img:"img/SetAnime/osito.jpg"},
+    {id:7, nombre:"Set Anime", precio:15000, cantidad:1, img:"img/SetAnime/setanime.jpg"},
+    {id:8, nombre:"Taza Cuerno", precio:6000, cantidad:1, img:"img/Tazas/taza.jpg"},
+    {id:9, nombre:"Taza Two", precio:6200, cantidad:1, img:"img/Tazas/tazatwo.jpg"},
+    {id:10, nombre:"Taza Three", precio:6500, cantidad:1, img:"img/Tazas/tazathree.jpg"},
+    {id:11, nombre:"Taza Four", precio:7000, cantidad:1, img:"img/Tazas/tazafour.jpg"},
+    {id:12, nombre:"Taza Five", precio:5800, cantidad:1, img:"img/Tazas/tazafive.jpg"},
+    {id:13, nombre:"Taza Six", precio:5000, cantidad:1, img:"img/Tazas/tazasix.jpg"}
+    ];
